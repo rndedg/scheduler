08886799514f9568c2ff0c101da761a39b152e5d
Gord L <rndedg@gmail.com>
2022-11-09 11:28:50 -0800

    Add helper function folder with selectors.js file to more easily manage helper functions

---
+++ src/helpers/selectors.js
@@ -0,0 +1,31 @@
+export function getAppointmentsForDay(state, day) {
+  let results = [];
+
+  let filteredDays = state.days.find(selectedDay => selectedDay.name === day);
+
+  if (!filteredDays) {
+    return [];
+  }
+
+  for (const appointmentID of filteredDays.appointments) {
+    if (state.appointments[appointmentID]) {
+      results.push(state.appointments[appointmentID]);
+    }
+  }
+
+  return results;
+
+}
+
+export function getInterview(state, interview) {
+  const results = {};
+
+  if (interview) {
+    results["student"] = interview.student;
+    results["interviewer"] = state.interviewers[interview.interviewer];
+  } else {
+    return null;
+  }
+  
+  return results;
+}
